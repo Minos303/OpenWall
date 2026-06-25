7010cd95dc83db1a31b2829a52128f6e5eaed975
Liam <liamhendres@gmail.com>
2026-06-25 12:49:06 +1200

    Update project files

--- Pit Wall Project/PitWall_Main.py
+++ Pit Wall Project/PitWall_Main.py
@@ -10,15 +10,21 @@
 #cd OpenWall
 #git pull origin main
 
-#Linux Run: python "Pit Wall Project/PitWall_Main.py"
+#Linux Run: 
+#python -m venv venv
+#source venv/bin/activate.fish
+#pip install pick requests panda
+#python "Pit Wall Project/PitWall_Main.py"
 
-#Create Virtual Environment: 
-# python -m venv venv
-# source venv/bin/activate.fish
 
+
+
+
+from operator import index
 
 
 import requests
+import urllib.error
 import pandas as pd
 import matplotlib.pyplot as plt
 from urllib.request import urlopen
@@ -26,10 +32,10 @@ import json
 import urllib.parse
 import urllib.request
 from pick import pick
-import tkinter
-import customtkinter
-import tkinter as tk
-import customtkinter as ctk  
+import os
+from flask import Flask, render_template
+import pandas as pd
+from datetime import datetime, timedelta
 
 
 
@@ -43,60 +49,13 @@ country_selection = ""
 year_selection = ""
 session_selection = ""
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("blue")
+app = Flask(__name__)
 
-Intro_app = customtkinter.CTk()
-Intro_app.title("F1 Pit Wall App")
-Intro_app.geometry("400x300")
-
-title = customtkinter.CTkLabel(Intro_app, text="Open Pit Wall")
-title.pack(pady=10, padx=10)
-
-def user_country_dropdown(country_selection):
-    user_country = country_selection
-    print(f"Selected Country: {user_country}")
-
-def user_year_dropdown(year_selection):
-    user_year = year_selection
-    print(f"Selected Year: {user_year}")
-
-def user_session_dropdown(session_selection):
-    user_session = session_selection
-    print(f"Selected Session: {user_session}")
-
-def submit_button():
-    user_country = country_dropdown_widget.get()
-    user_year = year_dropdown_widget.get()
-    user_session = session_dropdown_widget.get()
-    print(f"Session Selected: {user_country}, {user_session}, {user_year}")
-
-country_dropdown_widget = ctk.CTkOptionMenu(
-    Intro_app,
-    values=["Sakhir", "Jeddah", "Melbourne", "Baku", "Miami", "Imola", "Monte Carlo", "Catalunya", "Montreal", "Spielberg", "Silverstone", "Hungaroring", "Spa-Francorchamps", "Zandvoort", "Monza", "Singapore", "Suzuka", "Lusail", "Austin", "Mexico City", "Interlagos", "Las Vegas", "Yas Marina Circuit"],
-    command=user_country_dropdown
-)
-country_dropdown_widget.pack(pady=10, padx=10)
-country_dropdown_widget.set("Sakhir")
-
-year_dropdown_widget = ctk.CTkOptionMenu(
-    Intro_app,
-    values=["2020", "2021", "2022", "2023"],
-    command=user_year_dropdown
-)
-year_dropdown_widget.pack(pady=10, padx=10)
-year_dropdown_widget.set("2022")
-
-session_dropdown_widget = ctk.CTkOptionMenu(
-    Intro_app,
-    values=["Qualifying", "Race", "Practice 1", "Practice 2", "Practice 3", "Sprint Qualifying", "Sprint Race"],
-    command=user_session_dropdown
-)
-session_dropdown_widget.pack(pady=10, padx=10)
-session_dropdown_widget.set("Race")
-
-button = ctk.CTkButton(Intro_app, text="Next", command=submit_button)
-button.pack(pady=10, padx=10)
+@app.route('/')
+def main_dashboard():
+    driver = user_driver
+    team = user_team
+    return render_template('index.html', user_driver=driver, user_team=team)
 
 
 
@@ -118,62 +77,62 @@ def get_session_key(year, circuit_short_name, session_name):
     return  session_response.json()[0]["session_key"]
 
 
-gui_true = input("Do you want to use the GUI? (y/n): ")
-if gui_true == "y":
-    Intro_app.mainloop()
-
-else:
-#User Input
-    selection_country = ["Sakhir", "Jeddah", "Melbourne", "Baku", "Miami", "Imola", "Monte Carlo", "Catalunya", "Montreal", "Spielberg", "Silverstone", "Hungaroring", "Spa-Francorchamps", "Zandvoort", "Monza", "Singapore", "Suzuka", "Lusail", "Austin", "Mexico City", "Interlagos", "Las Vegas", "Yas Marina Circuit"]
-    user_country, index = pick(selection_country, "Select the Gran Prix: ")
-    print(f"Gran Prix: {user_country} (index: {index})")
 
 
-    selection_year = ["2020", "2021", "2022", "2023", "2024"]
-    user_year, index = pick(selection_year, "Select the Gran Prix: ")
-    print(f"Gran Prix: {user_year} (index: {index})")
+
+    #User Input
+selection_country = ["Sakhir", "Jeddah", "Melbourne", "Baku", "Miami", "Imola", "Monte Carlo", "Catalunya", "Montreal", "Spielberg", "Silverstone", "Hungaroring", "Spa-Francorchamps", "Zandvoort", "Monza", "Singapore", "Suzuka", "Lusail", "Austin", "Mexico City", "Interlagos", "Las Vegas", "Yas Marina Circuit"]
+user_country, index = pick(selection_country, "Select the Gran Prix: ")
+print(f"Gran Prix: {user_country} (index: {index})")
 
 
-    selection_session = ["Qualifying", "Race", "Practice 1", "Practice 2", "Practice 3", "Sprint Qualifying", "Sprint Race"]
-    user_session, index = pick(selection_session, "Select the Gran Prix: ")
-    print(f"Gran Prix: {user_session} (index: {index})")
+selection_year = ["2020", "2021", "2022", "2023", "2024"]
+user_year, index = pick(selection_year, "Select the Gran Prix: ")
+print(f"Gran Prix: {user_year} (index: {index})")
 
 
-    user_driver = input("Enter the driver number: ")
-    print (f"Session Selected:{user_country}, {user_session}, {user_year}, {user_driver}")
-    print (f"Grabbing Key...")
-
-#Generate session key
-    key = get_session_key(user_year, user_country, user_session)
-    print (f"Target Session Key: {key}")
-
-    response = urlopen(f'https://api.openf1.org/v1/championship_drivers?session_key={key}&driver_number={user_driver}')
-    data = json.loads(response.read().decode('utf-8'))
+selection_session = ["Qualifying", "Race", "Practice 1", "Practice 2", "Practice 3", "Sprint Qualifying", "Sprint Race"]
+user_session, index = pick(selection_session, "Select the Gran Prix: ")
+print(f"Gran Prix: {user_session} (index: {index})")
 
 
-    response_driver = urlopen(f'https://api.openf1.org/v1/drivers?driver_number={user_driver}&session_key={key}')
-    data_driver = json.loads(response_driver.read().decode('utf-8'))
+user_driver = input("Enter the driver number: ")
+print (f"Session Selected:{user_country}, {user_session}, {user_year}, {user_driver}")
+print (f"Grabbing Key...")
+
+    #Generate session key
+key = get_session_key(user_year, user_country, user_session)
+print (f"Target Session Key: {key}")
+
+response = urlopen(f'https://api.openf1.org/v1/championship_drivers?session_key={key}&driver_number={user_driver}')
+data = json.loads(response.read().decode('utf-8'))
 
 
-    driver_name = data_driver[0]['first_name']
-    driver_surname = data_driver[0]['last_name']
-    driver_acronym = data_driver[0]['name_acronym']
-    driver_standing = data[0]['position_current']
-    driver_team = data_driver[0]['team_name']
+response_driver = urlopen(f'https://api.openf1.org/v1/drivers?driver_number={user_driver}&session_key={key}')
+data_driver = json.loads(response_driver.read().decode('utf-8'))
 
-    team_url = urllib.parse.quote(driver_team)
 
-    print(f"Driver Name: {driver_name} {driver_surname}")
-    print(f"Driver Acronym: {driver_acronym}")
-    print(f"Driver Number: {user_driver}")
-    print(f"Current Standings: {driver_standing}")
-    print(f"Current Team: {driver_team}")
+driver_name = data_driver[0]['first_name']
+driver_surname = data_driver[0]['last_name']
+driver_acronym = data_driver[0]['name_acronym']
+driver_standing = data[0]['position_current']
+driver_team = data_driver[0]['team_name']
 
-    response = urlopen(f'https://api.openf1.org/v1/championship_teams?session_key={key}&team_name={team_url}')
-    data_team = json.loads(response.read().decode('utf-8'))
+team_url = urllib.parse.quote(driver_team)
 
-    team_standing = data_team[0]['position_current']
-    print(f"Current Team Standings: {team_standing}")
+print(f"Driver Name: {driver_name} {driver_surname}")
+print(f"Driver Acronym: {driver_acronym}")
+print(f"Driver Number: {user_driver}")
+print(f"Current Standings: {driver_standing}")
+print(f"Current Team: {driver_team}")
+
+response = urlopen(f'https://api.openf1.org/v1/championship_teams?session_key={key}&team_name={team_url}')
+data_team = json.loads(response.read().decode('utf-8'))
+
+team_standing = data_team[0]['position_current']
+print(f"Current Team Standings: {team_standing}")
+
+
 
  
 
@@ -185,8 +144,44 @@ current_data = json.loads(response.read().decode('utf-8'))
 
 
 date = current_data[0]['date']
+print (f"{date}")
+dt_date = datetime.fromisoformat(date)
 
-response = urlopen(f'https://api.openf1.org/v1/car_data?driver_number=1&session_key=7779&date=2023-03-19T16:01:03.621000+00:00')
-current_data = json.loads(response.read().decode('utf-8'))
-print(current_data)
+for i in range(30):
+    print(type(date))
+    print(date)
+    positive_time = dt_date + timedelta(seconds=0.05)
+    negative_time = dt_date - timedelta(seconds=0.05)
+
+
+
+    params = {
+    "driver_number": user_driver,
+    "session_key": key,
+    "date>": negative_time.isoformat(),
+    "date<": positive_time.isoformat()
+    }
+
+    query_string = urllib.parse.urlencode(params)
+
+    try: 
+        base_url = "https://api.openf1.org/v1/car_data"
+        full_url = f"{base_url}?{query_string}"
+
+    response = urlopen(full_url)
+
+    #response = urlopen(f'https://api.openf1.org/v1/car_data?driver_number={user_driver}&session_key={key}&date={date}>= {negative_time.isoformat()}&date<={positive_time.isoformat()}')
+    current_data = json.loads(response.read().decode('utf-8'))
+    current_speed = current_data[0]['speed']
+    print(f"Current Speed: {current_speed}")
+    dt_date = dt_date + timedelta(seconds=0.1)
+
+
+
+#if __name__ == '__main__':
+    #get_session_key(user_year, user_country, user_session)
+    #app.run()
+
+
+
 
